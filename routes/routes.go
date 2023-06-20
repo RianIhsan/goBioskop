5d910a559d7b20295d9b5f5ea9cb3c27ae1cc003
RianIhsan <rianwhatsup@gmail.com>
2023-06-20 22:24:16 +0700

    update routes

--- routes/routes.go
+++ routes/routes.go
@@ -6,9 +6,9 @@ import (
 )
 
 func InitRoute(app *fiber.App) {
-  app.Post("/api/create", controllers.Add)
-  app.Get("/api/reads", controllers.GetAll)
-  app.Get("/api/read/:id", controllers.GetById)
-  app.Put("/api/update/:id", controllers.Update)
-  app.Delete("/api/delete/:id", controllers.Delete)
+  app.Post("/api/films", controllers.Add)
+  app.Get("/api/films", controllers.GetAll)
+  app.Get("/api/films/:id", controllers.GetById)
+  app.Put("/api/films/:id", controllers.Update)
+  app.Delete("/api/films/:id", controllers.Delete)
 }
